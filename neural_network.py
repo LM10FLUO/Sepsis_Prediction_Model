010eb1847c649fb2c1cfefae949fb265d46d4ce0
LM10FLUO <felixluo12@gmail.com>
2026-08-08 19:47:57 +0100

    feat(nn) create lstm model

---
+++ neural_network.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from datasets import SepsisDataset
+from torch.nn.utils.rnn import pack_padded_sequence
+
+"""
+LSTM Model Theory:
+The LSTM Model builds on the RNN model, addressing its key issue: the vanishing/exploding gradient problem. As the RNN
+model involves a feedback loop that uses a weight, this causes this to exponentially decrease or increase, affecting
+gradient descent. The LSTM model uses 3 inputs: the long term memory input (cell state), short term memory input (hidden 
+state) and the input. Instead of relying on a feedback loop, the percentage of long term memory coupled with the 
+short term memory ensures we can use previous data to influence future predictions without the issue seen with RNNs
+"""
+
+class PredictionModel(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=40, hidden_size=64, num_layers=1, batch_first=True)
+        self.fc1 = nn.Linear(in_features=64, out_features=1)
+
+    def forward(self, x: torch.Tensor, lengths: torch.Tensor) -> torch.Tensor:
+        """
+            Pack padded tensors to remove redundant data, compressing tensors in each batch into a 1D array alongside
+            the original tensor size so the computer can correctly "decompress" the input data
+        """
+        x_packed = pack_padded_sequence(input=x, lengths=lengths.cpu(), batch_first=True, enforce_sorted=False)
+
+        h_n_collection, (h_n, c_n) = self.lstm(x_packed)
+        out = h_n[-1]
+        out = self.fc1(out)
+
+        return out
